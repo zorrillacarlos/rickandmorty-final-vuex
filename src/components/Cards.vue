50f110005a618e4efdb5ab3490bf543816d05710
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2022-05-30 10:48:34 -0400

    cards with state

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container">
     <div class="row d-flex justify-content-center">
-      <div class="card col-2 m-4 p-0" style="width: 25%" v-for="item in charactersProps" :key="item.id">
+      <div
+        class="card col-2 m-4 p-0"
+        style="width: 25%"
+        v-for="item in characters"
+        :key="item.id"
+      >
         <img :src="item.image" class="card-img-top" alt="item.name" />
         <div class="card-body">
-          <h5 class="card-title">{{item.name}}</h5>
+          <h5 class="card-title">{{ item.name }}</h5>
           <p class="card-text">
-            {{item.location.name}}
+            {{ item.location.name }}
           </p>
-          <p class="btn btn-primary">{{item.status}}</p>
+          <p class="btn btn-primary">{{ item.status }}</p>
         </div>
       </div>
     </div>
@@ -16,13 +21,11 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "Cards",
-  props: {
-    charactersProps: {
-      type: Array,
-      required: true,
-    },
+  computed: {
+    ...mapState("charactersRAM", ["characters"]),
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
     <h1>RICK AND MORTY API</h1>
-    <Cards :charactersProps="characters" />
+    <Cards/>
   </div>
 </template>
 
 <script>
 import Cards from "@/components/Cards.vue";
-import { mapState } from "vuex";
+
 export default {
   name: "Home",
   components: {
     Cards,
   },
-  computed: {
-    ...mapState("charactersRAM", ["characters"]),
-  },
-
   created() {
     this.$store.dispatch("charactersRAM/getCharacters");
   },
